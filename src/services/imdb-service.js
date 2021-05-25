abf74bbd7ae04ee1100d1cff61565619018e03b0
ZaidRehman Qureshi <zaidqureshi@gofynd.com>
2021-05-25 20:57:18 +0530

    minor changes

--- src/services/imdb-service.js
+++ src/services/imdb-service.js
@@ -1,4 +1,8 @@
-export const getMovies = (search) => {
+const getMovies = (search) => {
   return fetch(`http://www.omdbapi.com/?apikey=428abbdc&s=${search}`)
     .then(res => res.json())
+}
+
+export default {
+  getMovies
 }
